5512f8a48f70de367f85d1fd863e8446f165457f
thinthinnaingappleph <69711699+thinthinnaingappleph@users.noreply.github.com>
2020-08-15 17:32:47 +0630

    Update email.php

--- email.php
+++ email.php
@@ -1,12 +1,3 @@
 <?php 
 echo "sending email";
 echo "another email send";
-echo "third line from cli";
-echo "third line from online";
-
-
-echo 'forth line from cli';
-echo "forth line from online";
-
-echo 'fifth line from online';
-echo 'fifth line from cli';
